08e2dacd853b7747407cd35886456fc2a64c9b4a
Avidor Turkewitz <aviturkewitz@gmail.com>
2019-07-24 21:20:26 -0400

    get through first section

--- readme.md
+++ readme.md
@@ -6,3 +6,7 @@
 - `python3 -m venv venv` - creates folder “venv” for virtual env
 - `. venv/bin/activate`     # hop into env
 - `pip install -r requirements.txt`
+
+## Working
+
+Left off at: https://scikit-learn.org/stable/tutorial/statistical_inference/supervised_learning.html
---
+++ statistical-learning.py
@@ -0,0 +1,13 @@
+from sklearn import datasets
+iris = datasets.load_iris()
+data = iris.data
+data.shape
+
+digits = datasets.load_digits()
+digits.images.shape
+
+import matplotlib.pyplot as plt
+plt.imshow(digits.images[-1], cmap=plt.cm.gray_r)
+plt.show()
+
+data = digits.images.reshape((digits.images.shape[0], -1))
